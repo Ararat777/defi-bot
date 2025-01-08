4658409681f0aa9c7c6daa507168429c170a1bb8
Daniel Pohosian <daniel.pohosian@talkable.com>
2025-01-08 14:41:16 +0200

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,24 @@
 module.exports = {
   apps : [
       {
-        name   : "listener",
-        script : "./src/listener.ts",
+        name   : "server1",
+        script : "./server.ts",
         interpreter: "tsx",
         log_file: "./logs/listener.log",
         pid_file: "./pids/listener.pid",
+        env: {
+          PORT: 3000,
+        }
       },
     {
-      name   : "poller",
-      script : "./src/tg_bot/poller.ts",
+      name   : "server2",
+      script : "./server.ts",
       interpreter: "tsx",
       log_file: "./logs/poller.log",
       pid_file: "./pids/poller.pid",
+      env: {
+        PORT: 3001,
+      }
     },
   ]
 }
